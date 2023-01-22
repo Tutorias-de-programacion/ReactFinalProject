a7006f89de7f0ad22e9cba62f2370b44570a2f97
Adonis De Paula <adonsfco@gmail.com>
2023-01-21 21:41:49 -0400

    [feat] Install vite basic files.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <h1>Welcome to reactFlix</h1>
+      <h2>Please read the readme file before do any change on this code.</h2>
+      <h2>Check the backlog here:</h2>
+      <a>https://www.notion.so/</a>
+      <p>For further information please ask in the whatsapp group. </p>
+    </>
+  );
+}
+
+export default App;
