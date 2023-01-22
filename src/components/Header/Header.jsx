c9526a071f1841a125d12b2089bce838a893de88
AdonsFCO <52143660+AdonsFCO@users.noreply.github.com>
2023-01-22 13:53:18 -0400

    Routing system (#6)
    * [Ti-03][Ref] Add routing and modify pages name
    This is for making the code more clear.
    
    * [Ti-03][Ref] Fix search name and delete movie sub/folder
    
    * [Ti-03][Ref] Add dynamic links and set page template
    
    * [Ti-03][Ref] Make the code more clear

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,11 @@
+import { Link /*Or NavLink */} from "react-router-dom";
+
+//Please use <Link> or <NavLink> instead of <a> for more information:
+//Link https://reactrouter.com/en/main/components/link
+//NavLink https://reactrouter.com/en/main/components/nav-link
+
 const Header = () => {
-    return (<h1>I'm a Header</h1>);
+    return (<h1>Here goes the header</h1>);
 }
  
 export default Header;
--- src/pages/favoritesPage/favorites.jsx
+++
@@ -1,5 +0,0 @@
-const favoritesPage = () => {
-    return (<h1>Favorites Page</h1>);
-}
- 
-export default favoritesPage
