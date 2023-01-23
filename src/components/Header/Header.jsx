fe8e7eb39f080ac954d080d6d9fdb84c10364947
Winser Espinal <winser500@gmail.com>
2023-01-23 12:36:17 -0400

    Home (#8)
    * [Ti-04][Feat] Add Home Page Component
    
    * [Ti-05][Feat] Add bootstrap header and will add globalStyles.css to /styles with some test variables
    
    * [Ti-05][Feat] We fought bootstrap, but bootstrap won

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,42 @@
-import { Link /*Or NavLink */} from "react-router-dom";
+import { Link /*Or NavLink */ } from "react-router-dom";
+import "./Header.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 //Please use <Link> or <NavLink> instead of <a> for more information:
 //Link https://reactrouter.com/en/main/components/link
 //NavLink https://reactrouter.com/en/main/components/nav-link
 
 const Header = () => {
-    return (<h1>Here goes the header</h1>);
-}
- 
+  return (
+    <Navbar className="navBg" expand="lg">
+      <Container fluid>
+        <Navbar.Brand className="navText" href="#">Reactflix</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          ></Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button>Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
 export default Header;
