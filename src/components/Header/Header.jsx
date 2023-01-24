9f2b96a8ab3785b1c02149c6fefb35e8121cec0c
Winser Espinal <winser500@gmail.com>
2023-01-23 21:02:37 -0400

    Header (#9)
    * [Feat][Ti-05] add sass, will add custom.scss to /styles. This was done in order to be able to modify basic bootstrap colors
    
    * [Ti-05][Feat] add https://react-icons.github.io/react-icons/ as our icon library
    
    * [Ti-05][Feat] Finished basic styling for header
    
    * [Ti-05][Feat] Removed node-sass

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import Container from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import { AiOutlineSearch, AiOutlineStar, AiFillStar } from "react-icons/ai";
 
 //Please use <Link> or <NavLink> instead of <a> for more information:
 //Link https://reactrouter.com/en/main/components/link
@@ -25,13 +25,14 @@ const Header = () => {
             navbarScroll
           ></Nav>
           <Form className="d-flex">
+          <Button variant="dark" className="txtColor"><AiOutlineSearch /></Button>
             <Form.Control
               type="search"
               placeholder="Search"
-              className="me-2"
+              className="me-2 form-field"
               aria-label="Search"
             />
-            <Button>Search</Button>
+            <Button variant="dark" className="txtColor"><AiFillStar className="navText" /></Button>            
           </Form>
         </Navbar.Collapse>
       </Container>
