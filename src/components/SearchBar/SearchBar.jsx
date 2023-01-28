3879050ea5c152aa7493a7190a4aa3608d88b3bd
WinserEX <winser500@gmail.com>
2023-01-27 20:15:11 -0400

    [Ti-05][Feat] Developed the visuals of the searchBar component and added it to the header

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,6 +1,22 @@
+import Form from "react-bootstrap/Form";
+import { AiOutlineSearch, AiFillStar } from "react-icons/ai";
+import Button from "react-bootstrap/Button";
+import "./SearchBar.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 const SearchBar = () => {
-    return (<h1>I'm a SearchBar
-</h1>);
+    return (
+        <Form className="d-flex">
+            <Button variant="dark" className="txtColor"><AiOutlineSearch /></Button>
+                <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2 form-field"
+                aria-label="Search"
+                />
+            <Button variant="dark" className="txtColor"><AiFillStar className="navText" /></Button>            
+        </Form>
+    );
 }
  
 export default SearchBar;
