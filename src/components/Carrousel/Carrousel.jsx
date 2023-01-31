db590d17a98ba444e272ec90d3a47eea5d185653
AdonsFCO <52143660+AdonsFCO@users.noreply.github.com>
2023-01-31 12:56:05 -0400

    Carrousel  (#14)
    * [Ti-07][Feat] Add basic functionability of the carrousel.
    
    * [Ti-07][Feat] Add basic functionability of the carrousel.
    
    * [Ti-07][Feat] Make the carrousel responsive and add some explanation of how to use it.

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -1,12 +1,85 @@
 import getMovieList from "../../hooks/getMovieList";
+import Carousel from "react-bootstrap/Carousel";
+import { CardGroup } from "react-bootstrap";
+import { Card } from "react-bootstrap";
+import { useEffect, useState } from "react";
+import "./Carrousel.css";
+import { Link } from "react-router-dom";
+const ReactFlixCarrousel = ({
+  category = "Popular",
+  page = 1,
+  title = "carrousel",
+  autoChange = true,
+}) => {
+  //Determine if is going to autoChange or not this line disables the carrousel animation autoChange
 
-const Carrousel = ({ category, page, title }) => {
+  let interval = 5000;
+  if (!autoChange) {
+    interval = null;
+  }
+
   //To get the data from the category
   const movies = getMovieList(category, page);
 
-  console.log(movies); //<-- You can see the current list of movies.
+  //To divide all the movies in group of the max amount selected
+  //To define the max quantity of card
+  const [maxAmountOfCards, setMaxAmountOfCards] = useState(6);
 
-  return <h1>{title}</h1>;
+  useEffect(() => {
+    //This script updates the amount of images each time that the image resize. It adds an event listener to the windows object.
+    const handleResize = () => {
+      if (window.innerWidth < 480) {
+        setMaxAmountOfCards(1);
+      } else if (window.innerWidth >= 480 && window.innerWidth < 992) {
+        setMaxAmountOfCards(5);
+      } else {
+        setMaxAmountOfCards(10);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const groupsOfMovies = movies.reduce((parts, item, index) => {
+    const movie = Math.floor(index / maxAmountOfCards);
+    if (!parts[movie]) {
+      parts[movie] = [];
+    }
+    parts[movie].push(item);
+    return parts;
+  }, []);
+
+  return (
+    <>
+      <h2>{title}</h2>
+      <Carousel indicators={false} slide={"next"} interval={interval}>
+        {groupsOfMovies.map((movieGroup, index) => {
+          return (
+            <Carousel.Item key={`group-${index}`}>
+              <CardGroup className="carrouselCard">
+                {movieGroup.map((movie) => {
+                  return (
+                    <Card key={movie.id}>
+                      <Link to={`movie/${movie.id}`}>
+                        <Card.Img
+                          src={`http://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                          className="carrouselCard--img"
+                        />
+                      </Link>
+                    </Card>
+                  );
+                })}
+              </CardGroup>
+            </Carousel.Item>
+          );
+        })}
+      </Carousel>
+    </>
+  );
 };
 
-export default Carrousel;
+export default ReactFlixCarrousel;
--- src/pages/homePage/homePage.jsx
+++ src/pages/homePage/homePage.jsx
@@ -1,12 +1,25 @@
+import ReactFlixCarrousel from "../../components/Carrousel/Carrousel";
 
 const HomePage = () => {
   return (
     <>
-        <h1>Welcome to reactFlix</h1>
-        <h2>Please read the readme file before do any change to this code.</h2>
-        <h2>Check the backlog here:</h2>
-        <a href="https://www.notion.so/">www.notion.so</a>
-        <p>For further information please ask in the whatsapp group. </p>
+      <h1>Welcome to reactFlix</h1>
+      <h2>Please read the readme file before do any change to this code.</h2>
+      <h2>Check the backlog here:</h2>
+      <a href="https://www.notion.so/">www.notion.so</a>
+      <p>For further information please ask in the whatsapp group. </p>
+
+      {/*Keep in mind that the carrousel will work just if you use it in this way, but if you don't set the values. It has default values that will going to be added like this:
+      {
+        category ="popular",
+        page= 1,
+        title = "Popular",
+        autoChange = true
+      
+    added*/}
+      <ReactFlixCarrousel title="Popular" page={1} autoChange={false} />
+
+      <ReactFlixCarrousel title="Unpopular" />
     </>
   );
 };
