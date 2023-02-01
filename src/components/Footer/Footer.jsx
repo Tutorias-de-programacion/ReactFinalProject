8f522d38f72014ed639c2a71201effeb629ec91b
WinserEX <winser500@gmail.com>
2023-02-01 02:11:04 -0400

    [Ti-06][Feat] properly styled footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,6 +5,8 @@ import Col from 'react-bootstrap/Col';
 import { Button } from "react-bootstrap";
 import Image from 'react-bootstrap/Image'
 import logo from '../../Assets/img/reactflixLogo.png'
+import './Footer.css'
+import { BsLinkedin, BsGithub, BsInstagram, BsTwitter, BsYoutube } from "react-icons/bs"
 
 
 //Please use <Link> or <NavLink> instead of <a> for more information:
@@ -13,38 +15,38 @@ import logo from '../../Assets/img/reactflixLogo.png'
 
 const Footer = () => {
     return (
-        <>
-        <Container>
-            <Row>
-                <Col>Social Media</Col>
-            </Row>
-            <Row>
-                <Col xs={12} md={3}><p>About Reactflix</p></Col>
-                <Col xs={12} md={3}><p>Github Repo</p></Col>
-                <Col xs={12} md={3}><p>API</p></Col>
-                <Col xs={12} md={3}><p>Other Resources</p></Col>
-            </Row>
-            <Row>
-                <Col>
-                    <Button>Back to top</Button>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <p>©2022-2023 Reactflix Inc.</p>
-                </Col>
-            </Row>
-            <Row>
-                <Col></Col>
-                <Col>
-                    <Image src={logo} fluid alt="Netflix Logo" />
-                </Col>
-                <Col></Col>    
-            </Row>
-            
+        <Container fluid className="footBg pt-3">
+            <Container>
+                <Row>
+                    <Col>
+                        <p><BsLinkedin /> <BsGithub /> <BsInstagram /> <BsTwitter /> <BsYoutube /></p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs={12} md={3}><p>About Reactflix</p></Col>
+                    <Col xs={12} md={3}><p>Github Repo</p></Col>
+                    <Col xs={12} md={3}><p>API</p></Col>
+                    <Col xs={12} md={3}><p>Other Resources</p></Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <Button className="mb-3 b">Back to top</Button>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <p>©2022-2023 Reactflix Inc.</p>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col></Col>
+                    <Col>
+                        <Image src={logo} fluid alt="Netflix Logo" />
+                    </Col>
+                    <Col></Col>    
+                </Row>   
+            </Container>
         </Container>
-        
-        </>
     );
 }
  
