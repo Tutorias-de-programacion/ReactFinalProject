0dd933b742ee527fa3e1d8cb7530f16dc989289b
Adonis De Paula <adonsfco@gmail.com>
2023-02-01 22:57:15 -0400

    [Ti-09][Feat] Add single movie request.

--- API/index.js
+++ API/index.js
@@ -1,11 +1,24 @@
-import express from 'express'; import axios from 'axios'; 
-import cors from 'cors';
-import * as dotenv from 'dotenv';
-
-
-const port = process.env.PORT || 5000; 
-
+import express, { response } from "express";
+import axios from "axios";
+import cors from "cors";
+import * as dotenv from "dotenv";
+const port = process.env.PORT || 5000;
 const app = express();
-app.listen(port, ()=>{console.log(`Backend Running on port:${port}`)})
+dotenv.config();
+const api_key = process.env.TMDB_API_KEY;
+app.get("/", (req, res) => {
+  res.json("Hi bitch");
+});
+app.use(cors());
+app.get("/movie:id", (req, res) => {
 
+  axios.get(`https://api.themoviedb.org/3/movie:${req.params.id}?api_key=${api_key}&language=en-US`)
+    .then(serverResponse => {
+        const anw = serverResponse;
+        res.json(anw.data)
+    });
+});
 
+app.listen(port, () => {
+  console.log(`Backend Running on port:${port}`);
+});
