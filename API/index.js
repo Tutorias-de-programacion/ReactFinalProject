22d084f4175dcb0200ce0ac21b585c7e4a49cbf6
AdonsFCO <52143660+AdonsFCO@users.noreply.github.com>
2023-02-04 22:08:12 -0400

    single Movie page (#21)
    * [Ti-09][Feat] Add libraries and add custom scripts.
    This commit adds the libraries dotenv, express, concurrently, nodemon, cors and axios.
    Also adds the basic mask server. For hidding API/keys.
    
    * [Ti-09][Feat] Add single movie request.
    
    * [Ti-09][Feat] fix a little problem in the last commit.
    
    * [Ti-09][feat] Add API get movie functionability.
    make a new single movie mockup.
    
    * [Ti-09][Ref] Modify getSingleMovie to transform it in to a hook.
    
    * [Ti-09][Ref] Clarify how to use the data of useGetSingleMovie.
    
    * [Ti-09][Ref] Include information about the apiKey on the readme.md

---
+++ API/index.js
@@ -0,0 +1,43 @@
+import express from "express";
+import {
+  getMovieDetails,
+  getMovieDirector,
+  getMovieImages,
+  getMovieMainActors,
+  getMovieVideos,
+  getMovieWriter,
+} from "./singleMovieDataCollector.mjs";
+import cors from "cors";
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+
+app.get("/movie/:id", (req, res) => {
+  const id = req.params.id;
+  Promise.all([
+    getMovieDetails(id),
+    getMovieDirector(id),
+    getMovieVideos(id),
+    getMovieWriter(id),
+    getMovieMainActors(id),
+    getMovieImages(id),
+  ]).then(([movieDetails, director, videos, writer, stars, images]) => {
+    res.json({
+      id: movieDetails.id,
+      title: movieDetails.original_title,
+      overview: movieDetails.overview,
+      main_poster: `http://image.tmdb.org/t/p/w500/${movieDetails.poster_path}`,
+      images,
+      director,
+      videos,
+      writer,
+      stars,
+    });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Backend Running on port:${port}`);
+});
