d263d8393ef79b3258324bde84d557fbabe68ecc
Adonis De Paula <adonsfco@gmail.com>
2023-03-02 14:14:54 -0400

    [Ti-16][feat] Add search movie functionality

--- src/pages/searchPage/searchPage.jsx
+++ src/pages/searchPage/searchPage.jsx
@@ -1,19 +1,37 @@
 import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import useSearchMovie from "../../hooks/useSearchMovie";
 
 const SearchPage = () => {
-  const { query, page: pageNo } = useParams();
-  const [movieList, amountOfPages] = useSearchMovie(query, pageNo);
+  const { query, pageNo } = useParams();
+  const [movieList, maxAmountOfPages] = useSearchMovie(query, pageNo);
+
+  let navigate = useNavigate();
+
+  function handleCLickNext() {
+    if (Number(pageNo) < maxAmountOfPages) {
+      navigate(`/search/${query}/page/${Number(pageNo) + 1}`);
+    }
+  }
+  function handleCLickPrev() {
+    if (Number(pageNo) > 1) {
+      navigate(`/search/${query}/page/${Number(pageNo) - 1}`);
+    }
+  }
 
   return (
     <>
       <div className="body">
         <div>
           <h2>To change page</h2>
-          <button onClick={() => handleCLick(false)}>Next</button>
+          <button onClick={handleCLickPrev}>Previous</button>
+          <button onClick={handleCLickNext}>Next</button>
+        <div className="mario"></div>
+          <h3>
+            The max amount of pages that you can use is {maxAmountOfPages}
+          </h3>
         </div>
-        <h1>Your current search is: {query}</h1>
+        <h3>Your current search is: {query}</h3>
         {movieList && JSON.stringify(movieList)}
       </div>
     </>
