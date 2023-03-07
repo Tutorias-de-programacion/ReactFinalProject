4fdf2be94ba589c753e6dbd1fa1738ea4cc4d786
jordislpm <jordislpm@gmail.com>
2023-03-07 11:04:28 -0400

    feat: first intercafe completed without buttons

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Button.css'
+// import { FiPlay, AiOutlineInfoCircle } from "react-icons/ai";
+
+
+const Button = () => {
+  return (
+    <button className={`button`}>
+    {/* <FiPlay />
+    <AiOutlineInfoCircle className="navText" /> */}
+    <h4>pepe</h4>
+    </button>
+  )
+}
+
+export default Button
--- src/pages/singleMoviePage/singleMoviePage.jsx
+++ src/pages/singleMoviePage/singleMoviePage.jsx
@@ -1,6 +1,10 @@
 import { useParams } from "react-router-dom";
+import { useEffect } from "react";
 import useGetSingleMovie from "../../hooks/useGetSingleMovie";
 import YoutubeVideo from "../../components/youtubeVideo/youtubeVideo";
+import './singleMoviePage.css'
+import Button from "../../components/Button/Button";
+
 
 
 import dataSchema from "./dataSchema.png";
@@ -13,31 +17,68 @@ const SingleMoviePage = () => {
   const { movieId } = useParams();
   const [movie] = useGetSingleMovie(movieId);
 
+  // useEffect(()=>{
+  //  console.log(movie)
+  //  console.log(movie.stars[0].name)
+  // },[movie]
+    
+  // )
   return (
     <>
-      <h1>Data Schema</h1>
-      <img src={dataSchema} width="100%" />
+      {/* <h1>Data Schema</h1>
+      <img src={dataSchema} width="100%" /> */}
       {/*I know this is obvious but if don't delete
        the data Schema when you finish working with this page
        or if you don't need it*/}
-      <h2>Here is an example of how you can use this data</h2>
-      <h2>Your movie id is {movieId}</h2>
+      {/* <h2>Here is an example of how you can use this data</h2>
+      <h2>Your movie id is {movieId}</h2> */}
        
       {movie && (
         <div>
-          <h3>Movie title: {movie.title}</h3>
-          <h3>Movie Overview</h3>
-          <p>{movie.overview}</p>
-          <h3>The movie poster</h3>
-          <img src={movie.main_poster} />
-          <YoutubeVideo videoKey={movie.videos[0].key}/>
+          <div className="SinglePage_main">
+                <div className="SinglePage_main_left">
+                    <h3>{movie.title.split(" ")[0]}</h3>
+                    <h4>{movie.title.slice(1)}</h4>
+                    <p>{movie.overview}</p>
+                    <div>
+                      {/* <Button/> */}
+                    </div>
+                </div>
+                <div className="SinglePage_main_right">
+                  {movie.main_poster ? <img className="SinglePage_main_img" src={movie.main_poster}/>
+                  : <img className="SinglePage_main_img" src={movie.images.backdrops[0].path}/>}
+                  
+                  
+                </div>
+          </div>
+          <div className="SinglePage_section">
+                <div className="SinglePage_section_left">
+                  <div>Director</div>
+                  <div>{movie.director.name}</div>
+                  <div>Writer</div>
+                  <div>{movie.stars[0].name}</div>
+                  <div>Stars</div>
+                  <div>
+                  {movie.stars.map((star)=>{
+                    return <span key={star.credit_id}>| {star.name} | </span>
+                  })}
+                  </div>
+                </div>
+                <div className="SinglePage_section_right">
+                  <h3>TRAILER</h3>
+                  <iframe className="trailerVideo" src={`https://www.youtube.com/embed/${movie.videos[0].key}`} frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                      {/* <YoutubeVideo width="300" height="200" videoKey={movie.videos[0].key}/> */}
+                </div>
+          </div>
+          
+          {/*
           <h3>Other images that you can use from here:</h3>
 
-          {movie.images.backdrops &&
+          movie.images.backdrops &&
             movie.images.backdrops.map((backdrop, key) => {
               return <img src={backdrop.path} key={key}/>;
             })
-            /*Sometimes movies doesn't have backdrops, please consider 
+            Sometimes movies doesn't have backdrops, please consider 
             to use an alternative image when the image doesn't exist.
             */
             
