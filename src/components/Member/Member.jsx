31b26347bfc69e1fe096b3cdcbca2de6424e8cbd
jordislpm <jordislpm@gmail.com>
2023-03-28 11:42:36 -0400

    [feat] created member component

---
+++ src/components/Member/Member.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./member.css"
+import { Image } from 'react-bootstrap'
+import logo from "../../Assets/img/team/jordis.png"
+
+const Member = ({imgLink, name,linkedin}) => {
+
+    let link = `../../Assets/img/team/jordis.png`
+  return (
+    
+    <div className='member'>
+        <a  className='member_a' href={linkedin}>
+        <Image src={imgLink} fluid alt={name} className="pb-3 imgMember"/>
+        </a>
+        <p>{name}</p>
+    </div>
+  )
+}
+
+export default Member
