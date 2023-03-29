af997cdee2ff48829fd9293b0c81610c37733382
Adonis De Paula <adonsfco@gmail.com>
2023-03-28 20:56:34 -0400

    [feat]Include posters on movie lists request.

--- API/movieListDataCollector.mjs
+++ API/movieListDataCollector.mjs
@@ -13,20 +13,31 @@ async function getMovieList(category, page) {
   const movieListRaw = await axios.get(
     `https://api.themoviedb.org/3/movie/${category}?api_key=${api_key}&language=en-US&page=${page}`
   );
-  const movieList = await movieListRaw.data.results.map((movie) => {
-    return {
-      id: movie.id,
-      title: movie.original_title,
-      overview: movie.overview,
-      main_poster: `http://image.tmdb.org/t/p/w500${movie.poster_path}`,
-      votes: {
-        vote_average: movie.vote_average,
-        vote_count: movie.vote_count,
-      },
-    };
-  });
 
-  return {results: movieList, total_pages: movieListRaw.data.total_pages};
+  
+  const movieList = await Promise.all(
+    movieListRaw.data.results.map(async (movie) => {
+      const images = await axios.get(
+        `https://api.themoviedb.org/3/movie/${movie.id}/images?api_key=${api_key}`
+      );
+      
+      return {
+        id: movie.id,
+        title: movie.original_title,
+        overview: movie.overview,
+        main_poster: `http://image.tmdb.org/t/p/w500${movie.poster_path}`,
+        images: images.data,
+        votes: {
+          vote_average: movie.vote_average,
+          vote_count: movie.vote_count,
+        },
+      };
+    })
+  );
+  
+
+  console.log(movieList)
+  return { results: movieList, total_pages: movieListRaw.data.total_pages };
 }
 
 export default getMovieList;
